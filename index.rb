db518b940f3c549a559cd7091580494254513969
endokazuki <worldanalysis1014@gmail.com>
2024-02-20 13:44:31 +0900

    勉強ログ2024/02/20

--- index.rb
+++ index.rb
@@ -1,10 +1,10 @@
-head = "Ruby"
-tail = " on Rails"
+head = 'Ruby'
+tail = ' on Rails'
 # 文字列の結合
 head.concat tail
 # 変数headの文字列を逆順にする(変数の変更ではない)
 head.reverse
 # インデックス番号1から文字列"R"の位置を返す(インデックス番号0に位置する文字列は除外される)
-p head.index("R", 1)
+p head.index('R', 1)
 
 
