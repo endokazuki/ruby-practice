436f4dca7d92a8d4629855b5af5c052765930bac
endokazuki <worldanalysis1014@gmail.com>
2024-02-27 17:32:00 +0900

    勉強ログ2024/02/27

--- raise.rb
+++ raise.rb
@@ -12,3 +12,23 @@ rescue StandardError => e
   puts e.class   # => RuntimeError
   puts e.message # => 特定の例外が発生しました
 end
+
+count = 0
+
+begin
+  1 / count
+rescue NameError
+  count += 1
+  retry
+rescue NoMethodError
+  count += 2
+  retry
+rescue ZeroDivisionError
+  count += 3
+  retry
+else
+  puts 4
+# 例外処理で必ず実行される処理
+ensure
+  puts "count: #{count}"
+end
